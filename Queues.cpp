0c49de5fb620f2803d40c92d0c05e270d399eef9
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-12 15:37:48 +0700

    jika FRONT_position <= REAR_position, iterasi dar front hingga rear

--- Queues.cpp
+++ Queues.cpp
@@ -75,5 +75,34 @@ class Queues
         return;
         
     }
+
+    cout << "\nElement is the queue are...\n";
+
+    if (FRONT_position <= REAR_position)
+    {
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;  
+    }
+     else
+    {
+        while (FRONT_position <= max -1)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+
+         FRONT_position = 0;
+        
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;  
+        }
     }
 };
